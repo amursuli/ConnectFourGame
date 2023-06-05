b64a51ed7782785f20fd6796257501d7a766ebb1
mursuli <P4ssw0rd>
2023-06-05 17:19:34 -0400

    first commit

---
+++ ConnectFourGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ConnectFourGame;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ConnectFourGame.Shared.GameState>();
+
+await builder.Build().RunAsync();
+
